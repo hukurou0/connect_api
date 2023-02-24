4fba1118ff49526002e9a3044f6e4a0770179921
hukurou0 <junjun.0712.on@gmail.com>
2023-02-24 23:42:17 +0900

    add post

---
+++ post.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+#POST先URL
+url = "http://127.0.0.1:5000/api/signup"
+
+#JSON形式のデータ
+json_data = {
+            "username":"aa",
+            "password":"aa",
+            "department":"1"
+        }    
+
+#POST送信
+response = requests.post(
+    url,
+    json = json.dumps(json_data)    #dataを指定する
+    )
+
+res_data = response.json()
+print(res_data)
