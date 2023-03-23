47dd179c1dd770f559d65378caa3f9807bbe43c2
hukurou0 <junjun.0712.on@gmail.com>
2023-03-24 02:07:59 +0900

    add pack_func

---
+++ pack_func.py
@@ -0,0 +1,51 @@
+from flask import current_app
+from database_defined import app, db, get_key, increment_key
+from database_defined import (User, Admin, User_login, Login_limiter, OTP_table, Gakka, Subject, Taken, 
+                               Task, Old_task, Task_regist, Task_regist_kind, Manage_primary_key)
+from datetime import datetime, date, timedelta
+from pack_datetime_unixtime_serial import get_float_serial, get_int_serial, serial_to_str
+
+ctx = app.app_context()
+ctx.push()
+app = current_app
+
+#TaskレコードのリストからTaskEntityのリストを生成し返す関数
+def create_task_entity(tasks:list):
+    tasks_list = []
+    for task in tasks:
+        subject:Subject = Subject.query.filter_by(id=task.subject_id).one()
+        tasks_list += [
+            {
+                "subject_id":subject.id,
+                "subject_name": subject.subject_name,
+                "task_id": task.id,
+                "deadline_year":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%Y'),
+                "deadline_month":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%m'),
+                "deadline_day":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%d'),
+                "summary": task.summary,
+                "detail": task.detail,
+                "difficulty":task.difficulty
+            }
+        ]
+    return tasks_list
+                
+def create_task_entity_in_apitaskgetTasks(tasks:list):
+    tasks_dic = {}
+    today_serial = get_int_serial()
+    for task in tasks:
+        serial = task.serial
+        if serial >= today_serial:
+            subject:Subject = Subject.query.filter_by(id=task.subject_id).one()
+            tasks_dic[task.id] = {
+                    "subject_id":subject.id,
+                    "subject_name": subject.subject_name,
+                    "task_id": task.id,
+                    "deadline_year":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%Y'),
+                    "deadline_month":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%m'),
+                    "deadline_day":(datetime(1899,12,30) + timedelta(task.serial)).strftime('%d'),
+                    "summary": task.summary,
+                    "detail": task.detail,
+                    "difficulty":task.difficulty
+                }
+            
+    return tasks_dic
