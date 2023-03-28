f252396f6856addeca9cfa8ffa916fd24ed1a654
hukurou0 <junjun.0712.on@gmail.com>
2023-03-28 11:48:31 +0900

    fix Access-Control-Allow-Origin

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ login_manager.init_app(app)
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+  response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
